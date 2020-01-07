83a7aee252edd842f517ed62894aa0681554d94b
Cj-bc <cj.bc-sd@outlook.jp>
2020-01-08 00:42:17 +0900

    Adds 'ToImage' instance of 'Raster'

--- src/Graphics/Asciiart/Data/Raster.hs
+++ src/Graphics/Asciiart/Data/Raster.hs
@@ -11,12 +11,19 @@ module Graphics.Asciiart.Data.Raster
 (
 
 ) where
-import Data.Vector
+import qualified Data.Vector as V
+import Data.List.Split (chunksOf)
 import Graphics.Vty
-
+import Graphics.Asciiart.Type
 
 -- | Raster Style Ascii art
-data Raster = Raster { displayText :: Vector (Char, Attr) -- ^ Vector of data
+data Raster = Raster { displayText :: V.Vector (Char, Attr) -- ^ Vector of data
                      , width       :: Int -- ^ Width of the ASCII art
                      }
 
+instance ToImage Raster where
+    toImage (Raster txt w)= foldl1 (<->) rows
+        where
+            chars = map (\(c, a) -> char a c) $ V.toList txt
+            rows  = map mkrow $ chunksOf w chars
+            mkrow = foldl1 (<|>)
